aa0ce9c47d48f015335383e523d60df1cb76d58d
Himal <itissubedi@gmail.com>
2025-12-24 11:42:27 +0545

    update db name

--- src/config/index.ts
+++ src/config/index.ts
@@ -22,6 +22,6 @@ export const dbConfig:DBConfig ={
     DB_HOST: process.env.DB_HOST || 'localhost',
     DB_USER: process.env.DB_USER || 'root',
     DB_PASSWORD: process.env.DB_PASSWORD || 'root',
-    DB_NAME: process.env.DB_NAME || 'airbnb',
+    DB_NAME: process.env.DB_NAME || 'airbnb_booking',
 }
 
