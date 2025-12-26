fd77ba294558a8eda3236781b6c1f6f24b74e1bc
Himal <itissubedi@gmail.com>
2025-12-26 10:40:04 +0545

    feat(BookingService): add dto and update controller and service

--- BookingService/src/controllers/booking.controller.ts
+++ BookingService/src/controllers/booking.controller.ts
@@ -2,12 +2,7 @@ import { createBookingService } from "../services/booking.service"
 import { Request, Response } from "express"
 
 export const createBookingHandler = async (req: Request, res: Response)=>{
-    const booking = await createBookingService(
-        req.body.userId,
-        req.body.hotelId,
-        req.body.totalGuests,
-        req.body.bookingAmount
-    );
+    const booking = await createBookingService(req.body);
     res.status(201).json({
         bookingId: booking.bookingId,
         idempotencyKey: booking.idempotencyKey,
--- BookingService/src/services/booking.service.ts
+++ BookingService/src/services/booking.service.ts
@@ -1,19 +1,14 @@
+import { CreateBookingDTO } from "../dto/booking.dto";
 import { confirmBooking, createBooking, createIdempotencyKey, finalizeIdempotencyKey, getIdempotencyKey } from "../repositories/booking.repository";
 import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
 import { generateIdempotencyKey } from "../utils/generateIdempotencyKey";
 
-export async function createBookingService(
-   userId: number,
-   hotelId: number,
-   totalGuests: number,
-   bookingAmount: number,
-
-){
+export async function createBookingService(createBookingDTO: CreateBookingDTO){
     const booking = await createBooking({
-        userId,
-        hotelId,
-        totalGuest: totalGuests,
-        bookingAmount: bookingAmount,
+        userId: createBookingDTO.userId,
+        hotelId: createBookingDTO.hotelId,
+        totalGuest: createBookingDTO.totalGuests,
+        bookingAmount: createBookingDTO.bookingAmount,
     });
     const idempotencyKey = generateIdempotencyKey();
 
