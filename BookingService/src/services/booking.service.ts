ae0382b111b1412c00d1088410008a2987dad317
Himal <itissubedi@gmail.com>
2025-12-26 10:02:05 +0545

    feat(BookingService): add service  layer

--- BookingService/src/services/booking.service.ts
+++ BookingService/src/services/booking.service.ts
@@ -1,7 +1,33 @@
-export async function createBookingService() {
-    
-}
+import { confirmBooking, createBooking, finalizeIdempotencyKey, getIdempotencyKey } from "../repositories/booking.repository";
+import { BadRequestError, NotFoundError } from "../utils/errors/app.error";
+import { generateIdempotencyKey } from "../utils/generateIdempotencyKey";
 
-export async function finalizeBookingService() {
+export async function createBookingService(
+   userId: number,
+   hotelId: number,
+   totalGuests: number,
+   bookingAmount: number,
+
+){
+    const booking = await createBooking({
+        userId,
+        hotelId,
+        totalGuest: totalGuests,
+        bookingAmount: bookingAmount,
+    });
+    const idempotencyKey = generateIdempotencyKey();
+
+}
+export async function finalizeBookingService(idempotencyKey: string) {
+    const idempotencyKeyData = await getIdempotencyKey(idempotencyKey);
+    if(!idempotencyKey){
+        throw new NotFoundError('Idempotency Key not found');
+    }
+    if(idempotencyKeyData.finalized){
+        throw new BadRequestError("Idempotency Key already finilized")
+    }
+    const booking = await confirmBooking(idempotencyKeyData.bookingId);
+    await finalizeIdempotencyKey(idempotencyKey);
     
+    return booking;
 }
