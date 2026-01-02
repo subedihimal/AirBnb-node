9c2a3e05ebc8f3eedcf1a1f1854067ea92d436ea
Himal <itissubedi@gmail.com>
2026-01-02 12:25:35 +0545

    add(NotificationService): add  emailproducer

--- NotificationService/src/queues/mailer.queue.ts
+++ NotificationService/src/queues/mailer.queue.ts
@@ -1,8 +1,8 @@
 import {Queue} from 'bullmq';
 import { getRedisConnectionObject } from '../config/redis.config';
 
-export const Mailer_Queue = "queue:mailer";
+export const MailerQueue = "queue:mailer";
 
-export const mailer_Queue = new Queue(Mailer_Queue, {
+export const mailerQueue = new Queue(MailerQueue, {
     connection: getRedisConnectionObject(),
 });
