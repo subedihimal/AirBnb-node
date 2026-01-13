b08efd0ef2a0305230687dc2ab73a9a0bdc80787
subedihimal <itissubedi@gmail.com>
2026-01-13 08:07:32 +0545

    update(HotelService): hotelservice to use new updated hotelrepository

--- HotelService/src/services/hotel.service.ts
+++ HotelService/src/services/hotel.service.ts
@@ -1,23 +1,25 @@
 import { createHotelDTO } from "../dto/hotel.dto";
-import { createHotel, getHotelById, getAllHotel, softDeleteHotel } from "../repositories/hotel.repository";
+import { HotelRepository } from "../repositories/hotel.repository";
+
+const hotelRepository = new HotelRepository();
 
 export async function createHotelService(hotelData: createHotelDTO){
-    const hotel = await createHotel(hotelData);
+    const hotel = await hotelRepository.create(hotelData);
     return hotel;
 }
 
 export async function getHotelByIdService(id: number){
-    const hotel = await getHotelById(id);
+    const hotel = await hotelRepository.findById(id);
     return hotel;
 }
 
 export async function getAllHotelService(){
-    const hotel = await getAllHotel();
+    const hotel = await hotelRepository.findAll();
     return hotel;
 }
 
 export async function softDeleteHotelService(id :number) {
-    const hotel = await softDeleteHotel(id);
+    const hotel = await hotelRepository.softDelete(id);
     return hotel;
     
 }
