263b62f5c436bbe3f94f817a220deedd98bb3c1d
subedihimal <itissubedi@gmail.com>
2026-02-11 21:55:46 +0545

    add(AuthInGo): migration,seeder for role_permissions_table

--- AuthInGo/db/migrations/20260211133326_create_role_permissions_table.sql
+++ AuthInGo/db/migrations/20260211133326_create_role_permissions_table.sql
@@ -1,9 +1,21 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXISTS role_permissions(
+    id SERIAL PRIMARY KEY,
+    role_id INT NOT NULL,
+    permission_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE CASCADE,
+    FOREIGN KEY (permission_id) REFERENCES permissions(id) ON DELETE CASCADE
+);
 -- +goose StatementEnd
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT 1, id FROM permissions; -- assuming role 1 is admin and it has all permissions
 
+INSERT INTO role_permissions (role_id, permission_id)
+SELECT 2, id FROM permissions where name IN ('user;read');
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DELETE TABLE IF EXISTS role_permissions;
 -- +goose StatementEnd
