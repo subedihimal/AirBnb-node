5585551d3bcbbb57cd86e3abf4ae1692b3599369
subedihimal <itissubedi@gmail.com>
2026-02-11 21:01:03 +0545

    add(AuthInGO): migration, seeder for create role and permissions table

--- AuthInGo/db/migrations/20260211133258_create_permissions_table.sql
+++ AuthInGo/db/migrations/20260211133258_create_permissions_table.sql
@@ -1,9 +1,31 @@
 -- +goose Up
 -- +goose StatementBegin
-SELECT 'up SQL query';
+CREATE TABLE IF NOT EXIST permissions(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL UNIQUE,
+    description TEXT,
+    resource VARCHAR(100) NOT NULL,
+    action VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+)
 -- +goose StatementEnd
+
+--Migration Seeder code
+INSERT INTO permissions (name, description, resource, action) VALUES
+('user:read', 'Permission to read user data', 'user', 'read'),
+('user:write', 'Permission to write user data', 'user', 'write'),
+('user:delete', 'Permission to delete user data', 'user', 'delete'),
+('role:read', 'Permission to read role data', 'role', 'read'),
+('role:write', 'Permission to write role data', 'role', 'write'),
+('role:delete', 'Permission to delete role data', 'role', 'delete'),
+('role:manage', 'Permission to manage role data', 'role', 'manage'),
+('permission:read', 'Permission to read permissions', 'permission', 'read'),
+('permission:write', 'Permission to write permissions', 'permission', 'write'),
+('permission:delete', 'Permission to delete permissions', 'permission', 'delete'),
+('permission:manage', 'Permission to manage permissions', 'permission', 'manage');
 
 -- +goose Down
 -- +goose StatementBegin
-SELECT 'down SQL query';
+DELETE TABLE IF EXISTS permission;
 -- +goose StatementEnd
