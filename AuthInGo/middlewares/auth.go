37fef4eb91252867878f49f78d6d206f8c996aa0
subedihimal <itissubedi@gmail.com>
2026-03-09 09:36:28 +0545

    fix(AuthInGO): role authentication

--- AuthInGo/middlewares/auth.go
+++ AuthInGo/middlewares/auth.go
@@ -62,6 +62,49 @@ func JWTAuthMiddleware(next http.Handler) http.Handler {
 
 }
 
+func RequireAllRoles(roles ...string) func(http.Handler) http.Handler {
+
+	// function that can create a middleware for checking the above set of roles
+
+	return func(next http.Handler) http.Handler {
+
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			userIdStr := r.Context().Value("userID").(string)
+			userId, err := strconv.ParseInt(userIdStr, 10, 64)
+			if err != nil {
+				http.Error(w, "Invalid user ID", http.StatusUnauthorized)
+				return
+			}
+
+			dbConn, dbErr := dbConfig.SetupDB()
+			if dbErr != nil {
+				http.Error(w, "Database connection error: "+dbErr.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			urr := repo.NewUserRoleRepository(dbConn)
+
+			hasAllRoles, hasAllRolesErr := urr.HasAllRoles(userId, roles)
+			fmt.Println("userid", userId, "roles", roles, "hasAllRoles", hasAllRoles)
+			if hasAllRolesErr != nil {
+				http.Error(w, "Error checking user roles: "+hasAllRolesErr.Error(), http.StatusInternalServerError)
+				return
+			}
+
+			if !hasAllRoles {
+				http.Error(w, "Forbidden: You do not have the required roles", http.StatusForbidden)
+				return
+			}
+
+			fmt.Println("User has all required roles:", roles)
+
+			next.ServeHTTP(w, r)
+		})
+	}
+
+}
+
 func RequireAnyRole(roles ...string) func(http.Handler) http.Handler {
 
 	return func(next http.Handler) http.Handler {
