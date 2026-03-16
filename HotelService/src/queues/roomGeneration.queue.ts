c19cae6b04677f0c448f31351fea59d965c8dc86
subedihimal <itissubedi@gmail.com>
2026-03-16 16:22:08 +0545

    fix(HotelService): redis error

---
+++ HotelService/src/queues/roomGeneration.queue.ts
@@ -0,0 +1,8 @@
+import {Queue} from 'bullmq';
+import { redisConnectionOptions } from '../config/redis.config';
+
+export const ROOM_GENERATION_QUEUE = "room-generation-queue";
+
+export const roomGenerationQueue = new Queue(ROOM_GENERATION_QUEUE  , {
+    connection: redisConnectionOptions,
+});
