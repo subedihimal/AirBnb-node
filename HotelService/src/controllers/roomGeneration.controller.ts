14155b43e3b3180f8973c9b028e4bd7f91abc6c2
subedihimal <itissubedi@gmail.com>
2026-03-16 17:51:26 +0545

    add(HotelService): routes for roomGeneration

--- HotelService/src/controllers/roomGeneration.controller.ts
+++ HotelService/src/controllers/roomGeneration.controller.ts
@@ -1,14 +1,16 @@
 import { success } from "zod";
 import { generateRooms } from "../services/roomGeneration.service"
 import { Request, Response } from "express";
+import { addRoomGenerationJobToQueue } from "../producers/roomGeneration.producer";
 
 export async function generateRoomHandler(req: Request, res: Response){
-    const result = await generateRooms(req.body);
+
+    await addRoomGenerationJobToQueue(req.body);
 
     res.status(200).json({
-        message: "Room generation job completed successfully",
+        message: "Room generation job added to queue",
         success: true,
-        data: result,
+        data: {},
     });
 
 }
--- HotelService/src/routers/index.ts
+++ HotelService/src/routers/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import pingRouter from "./ping.router";
 import hotelRouter from "./hotel.router";
+import roomGenerationRouter from "./roomGeneration.router";
+
 
 const app = express();
 
 app.use('/ping', pingRouter);
 app.use('/hotels',hotelRouter)
+app.use('/room-generation', roomGenerationRouter);
 
 export default app;
